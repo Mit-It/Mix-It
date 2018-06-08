17610e067019836d140f8c383b8baef6499500a9
JenHa <jenny.hauss@gmx.de>
2018-06-08 11:37:12 +0200

    [Feature] Rezeptbuch Detailansicht

--- resources/views/pages/recipebook.blade.php
+++ resources/views/pages/recipebook.blade.php
@@ -1,12 +1,24 @@
 @extends('layouts.main')
 @section('content')
     <h1>{{$recipebook->title}}</h1>
-    @foreach ($cocktails as $cocktail)
+    <div class="col-md-9 col-sm-12 mi-rp-cocktails">
         <dl class="dl-horizontal">
-            <dt class="">
-                <a href="cocktails/{{$cocktail->id}}" class="mi-primaryColor">{{$cocktail->title}}</a>
-            </dt>
-            <dd>{{$cocktail->description}}</dd>
+            @foreach ($recipebook->cocktails as $cocktail)
+
+                <dt>
+                    <a href="../cocktails/{{$cocktail->id}}" class="mi-primaryColor">{{$cocktail->title}}</a>
+                </dt>
+                <dd>
+                    {{$cocktail->description}}
+
+                </dd>
+
+            @endforeach
         </dl>
-    @endforeach
+    </div>
+
+@stop
+
+@section('header')
+    <link href="/css/recipebook.css" rel="stylesheet">
 @stop
