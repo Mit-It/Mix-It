9395884bdbdc083773c8ff2197f5b3efc71996dd
JenHa <jenny.hauss@gmx.de>
2018-06-18 11:22:11 +0200

    [Feature] Add rating model

---
+++ app/Rating.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+
+
+    protected $fillable = array('value');
+
+
+    public function cocktail(){
+        return $this->belongsTo(Cocktail::class, 'cocktail_id');
+    }
+
+}
